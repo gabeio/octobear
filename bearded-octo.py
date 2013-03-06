9e60a98b6524d16f6f990e81e4d5cf937ee34c6b
Gabe Del <me@gabe.io>
2013-03-06 18:21:23 -0500

    v0.1 error in urllib2.Request()

---
+++ bearded-octo.py
@@ -0,0 +1,18 @@
+import urllib2
+from bottle import *
+
+@post('/<port:int>/<scheme>/<website>/<path:path>') #for non-http port
+@post('/<scheme>/<website>/<path:path>') #for everything else
+def post_get(website,port=80,scheme="http",path=None):
+    params = request.query
+    get = ""
+    for p in params:
+        get += (p+"="+params[p]+"&")
+    get=get[:-1]
+    req = urllib2.Request(scheme+"://"+website+":"+port+"/"+path+"?"+get)
+    yield "OK"
+
+@route('/')
+def index():yield ""
+
+run()
