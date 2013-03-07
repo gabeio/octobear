bbc6107297cc90e4a3c81cb9a0deb85434121844
Gabe Del <me@gabe.io>
2013-03-07 03:36:01 -0500

    added unit-test.py commented out debugging yields in get_post()

--- octobear.py
+++ octobear.py
@@ -20,12 +20,12 @@ def post_get(website,port=80,scheme="http",path=None):
 def get_post(website,port=80,scheme="http",path=None):
 	try:
 		params = request.query
-		yield str(params)
+		#yield str(params)
 		get = ""
 		for p in params:
 			get += str(str(p)+"="+str(params[p])+"&")
 		get=get[:-1]
-		yield str(get)
+		#yield str(get)
 		req = urllib2.urlopen(str(scheme)+"://"+str(website)+":"+str(port)+"/"+str(path)+"?"+str(get),"")
 		yield "OK"
 	except Exception,e: yield str(e)
---
+++ unit-test.py
@@ -0,0 +1,5 @@
+import octobear
+
+assert octobear.post_get() == "OK"
+assert octobear.get_post() == "OK"
+
