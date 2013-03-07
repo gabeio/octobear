f99bfffecb5ce13ed37c335df53f05755cc8deb8
Gabe Del <me@gabe.io>
2013-03-07 04:08:36 -0500

    changed returns back into yields & modified unit-test.py accordingly to handle yields

--- unit-test.py
+++ unit-test.py
@@ -1,4 +1,18 @@
 import octobear
 
-assert octobear.post_get("beta.shov.me",80,"http","passer") == "OK"
-assert octobear.get_post("beta.shov.me",80,"http","passer") == "OK"
+def post_get():
+	z = ""
+	x = octobear.post_get("beta.shov.me",80,"http","passer")
+	for y in x:
+		z+=y
+	return z
+
+def get_post():
+	z = ""
+	x = octobear.get_post("beta.shov.me",80,"http","passer")
+	for y in x:
+		z+=y
+	return z
+
+assert post_get() == "OK"
+assert get_post() == "OK"
