0a4d2aceed29fe47eb4a31a723248a7d2012adb5
Gabe Del <me@gabe.io>
2013-03-07 02:30:11 -0500

    added GET to POST does not post yet.

--- bearded-octo.py
+++ bearded-octo.py
@@ -5,6 +5,21 @@ import urllib2;from bottle import *
 def post_get(website,port=80,scheme="http",path=None):
 	try:
 		params = request.query
+		#yield str(params)
+		get = ""
+		for p in params:
+			get += str(str(p)+"="+str(params[p])+"&")
+		get=get[:-1]
+		#yield str(get)
+		req = urllib2.urlopen(str(scheme)+"://"+str(website)+":"+str(port)+"/"+str(path)+"?"+str(get))
+		yield "OK"
+	except Exception,e: yield str(e)
+
+@get('/<port:int>/<scheme>/<website>/<path:path>')
+@get('/<scheme>/<website>/<path:path>')
+def get_post(website,port=80,scheme="http",path=None):
+	try:
+		params = request.query
 		yield str(params)
 		get = ""
 		for p in params:
